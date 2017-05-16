5419dc300436126854234ecece651906e4ccf25a
Carlos Castro <alu0100819847@ull.edu.es>
2017-05-16 21:23:41 +0100

    Completados test

--- test/tests.js
+++ test/tests.js
@@ -6,14 +6,14 @@ describe("Función comparar", function(){
     var mySecuencia = new Secuencia(5, "Primos");
     var result = mySecuencia.getSecuencia();
     result.should.be.equal("Secuencia no definida");
-  }
+  })
   it("No existen operaciones declaradas", function() {
     var mySecuencia = new Secuencia(5, "Primos");
     var prueba = {};
     prueba.getVals = function() {return [1, 2, 3]};
     var result = mySecuencia.operar(prueba, "Igual")
     result.should.be.equal("Operación no definida");
-  }
+  })
 
 })
 
