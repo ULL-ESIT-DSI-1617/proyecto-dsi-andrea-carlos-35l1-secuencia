808f352fbf47259551d45392bec0c5b2a59559b5
Carlos Castro <alu0100819847@ull.edu.es>
2017-05-16 20:10:11 +0100

    Solucionando errores en test

--- test/tests.js
+++ test/tests.js
@@ -1,4 +1,26 @@
+var Secuencia = require("../lib/Secuencia.js");
+var should = require("should")
+
+describe("Función comparar", function(){
+  it("No existen secuencias declaradas", function() {
+    var mySecuencia = new Secuencia(5, "Primos");
+    var result = mySecuencia.getSecuencia();
+    result.should.be.equal("Secuencia no definida");
+  }
+  it("No existen operaciones declaradas", function() {
+    var mySecuencia = new Secuencia(5, "Primos");
+    var prueba = {};
+    prueba.getVals = function() {return [1, 2, 3]};
+    var result = mySecuencia.operar(prueba, "Igual")
+    result.should.be.equal("Operación no definida");
+  }
+
+})
+
+
+/*var chai = require('chai');
 var assert = chai.assert;
+
 suite('Secuencia', function() {
     test('No existen secuencias declaradas', function() {
         var mySecuencia = new Secuencia(5, "Primos");
@@ -12,3 +34,4 @@ suite('Secuencia', function() {
     });
 
 });
+*/
